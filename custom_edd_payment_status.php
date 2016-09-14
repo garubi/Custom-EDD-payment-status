834a7581493f2111f0a2b8ebd9545e858f302b5a
Stefano Garuti <sgaruti@gmail.com>
2016-09-14 13:32:50 +0200

    Customized with the Custom Statuses that I need

--- custom_edd_payment_status.php
+++ custom_edd_payment_status.php
@@ -27,8 +27,8 @@ License:
  */
 function sww_add_edd_payment_statuses( $payment_statuses ) {
 	
-	$payment_statuses['invoice_paid']	= 'Invoice Paid';
-	$payment_statuses['in_progress']	= 'Project in progress';
+	$payment_statuses['invoice_created']	= 'Fatturato';
+	//$payment_statuses['invoice_sent']	= 'Fattura inviata';
 
 	return $payment_statuses;	
 }
@@ -41,14 +41,15 @@ add_filter( 'edd_payment_statuses', 'sww_add_edd_payment_statuses' );
 function sww_register_post_type_statuses() {
 
 	// Payment Statuses
-	register_post_status( 'invoice_paid', array(
-		'label'                     => _x( 'Invoice Paid', 'Invoice paid payment status', 'sww-edd' ),
+	register_post_status( 'invoice_created', array(
+		'label'                     => _x( 'Fatturato', 'Invoice created payment status', 'sww-edd' ),
 		'public'                    => true,
 		'exclude_from_search'       => false,
 		'show_in_admin_all_list'    => true,
 		'show_in_admin_status_list' => true,
-		'label_count'               => _n_noop( 'Invoice Paid <span class="count">(%s)</span>', 'Invoice Paid <span class="count">(%s)</span>', 'sww-edd' )
+		'label_count'               => _n_noop( 'Fatturato <span class="count">(%s)</span>', 'Fatturato <span class="count">(%s)</span>', 'sww-edd' )
 	) );
+	/*
 	register_post_status( 'in_progress', array(
 		'label'                     => _x( 'Project in progress', 'In progress payment status', 'sww-edd' ),
 		'public'                    => true,
@@ -57,6 +58,7 @@ function sww_register_post_type_statuses() {
 		'show_in_admin_status_list' => true,
 		'label_count'               => _n_noop( 'In progress <span class="count">(%s)</span>', 'In progress <span class="count">(%s)</span>', 'sww-edd' )
 	)  );
+	*/
 }
 add_action( 'init', 'sww_register_post_type_statuses' );
 
@@ -66,8 +68,8 @@ add_action( 'init', 'sww_register_post_type_statuses' );
  */
 function sww_edd_payments_new_views( $views ) {
 	
-	$views['invoice_paid']	= sprintf( '<a href="%s">%s</a>', add_query_arg( array( 'status' => 'invoice_paid', 'paged' => FALSE ) ), 'Invoice paid' );
-	$views['in_progress']	= sprintf( '<a href="%s">%s</a>', add_query_arg( array( 'status' => 'in_progress', 'paged' => FALSE ) ), 'In progress' );
+	$views['invoice_paid']	= sprintf( '<a href="%s">%s</a>', add_query_arg( array( 'status' => 'invoice_created', 'paged' => FALSE ) ), 'Fatturato' );
+//	$views['in_progress']	= sprintf( '<a href="%s">%s</a>', add_query_arg( array( 'status' => 'in_progress', 'paged' => FALSE ) ), 'In progress' );
 	
 	return $views;
 
@@ -89,8 +91,8 @@ function sww_edd_bulk_status_dropdown( $actions ) {
  		
  		// Add our actions after the "Set To Cancelled" action
         if ( 'set-status-cancelled' === $key ) {
-            $new_bulk_status_actions['set-status-paid']			= 'Set To Invoice Paid';
-			$new_bulk_status_actions['set-status-in-progress']	= 'Set To In Progress';
+            $new_bulk_status_actions['set-status-invoice-created']			= 'Segna come Fatturato';
+		//	$new_bulk_status_actions['set-status-in-progress']	= 'Set To In Progress';
             // Add a $new_bulk_status_actions[key] = value; for each status you've added (in the order you want)
         }
     }
@@ -105,14 +107,14 @@ add_filter( 'edd_payments_table_bulk_actions', 'sww_edd_bulk_status_dropdown' );
  */
 function sww_edd_bulk_status_action( $id, $action ) {
 
-	if ( 'set-status-paid' === $action ) {
-		edd_update_payment_status( $id, 'invoice_paid' );
+	if ( 'set-status-invoice-created' === $action ) {
+		edd_update_payment_status( $id, 'invoice_created' );
 	}
-	
+	/*
 	if ( 'set-status-in-progress' === $action ) {
 		edd_update_payment_status( $id, 'in_progress' );
 	}
-	
+	*/
 }
 add_action( 'edd_payments_table_do_bulk_action', 'sww_edd_bulk_status_action', 10, 2 );
 
@@ -122,7 +124,8 @@ add_action( 'edd_payments_table_do_bulk_action', 'sww_edd_bulk_status_action', 1
  */
 function sww_edd_earnings_reporting_args( $args ) {
 
-	$args['post_status'] = array_merge( $args['post_status'], array( 'invoice_paid', 'in_progress' ) );
+	//$args['post_status'] = array_merge( $args['post_status'], array( 'invoice_paid', 'in_progress' ) );
+	$args['post_status'] = array_merge( $args['post_status'], array( 'invoice_created' ) );
 
 	return $args;
 }
@@ -131,7 +134,8 @@ add_filter( 'edd_get_earnings_by_date_args', 'sww_edd_earnings_reporting_args' )
 
 function sww_edd_sales_reporting_args( $args ) {
 
-	$args['post_status'] = array_merge( $args['post_status'], array( 'invoice_paid', 'in_progress' ) );
+	//$args['post_status'] = array_merge( $args['post_status'], array( 'invoice_paid', 'in_progress' ) );
+	$args['post_status'] = array_merge( $args['post_status'], array( 'invoice_created' ) );
 
 	return $args;
 }
